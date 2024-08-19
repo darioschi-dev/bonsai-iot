938c77df427e941e98769674105f2c4fa70a78e3
DeSmond-89 <schdra89@gmail.com>
2024-08-19 19:50:08 +0200

    Refactoring project with platformio

---
+++ .pio/build/esp32doit-devkit-v1/FrameworkArduino/libb64/cdecode.c.d
@@ -0,0 +1,3 @@
+.pio/build/esp32doit-devkit-v1/FrameworkArduino/libb64/cdecode.c.o: \
+ /Users/amministratore/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.c \
+ /Users/amministratore/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.h
