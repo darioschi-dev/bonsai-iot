26bcaa9b5bc9092c27192837679391dcabac6dd7
DeSmond-89 <schdra89@gmail.com>
2025-08-19 00:01:04 +0200

    📝 Aggiornato README con feature mancanti
    - Aggiunta menzione autenticazione via PIN e dark mode nella Web UI
    - Specificato che in modalità test viene generato automaticamente `config.test.json`
    - Corretta descrizione OTA (`/update` → `/upload-firmware`)

--- include/version_auto.h
+++ include/version_auto.h
@@ -1,4 +1,4 @@
 #pragma once
-#define FIRMWARE_VERSION "v1.4.8"
+#define FIRMWARE_VERSION "v1.4.9"
 #define FIRMWARE_COMMIT  "803ff62"
-#define FIRMWARE_BUILD   "2025-08-18 23:31:53"
+#define FIRMWARE_BUILD   "2025-08-19 00:00:41"
